9ff0f4990864e099185160820f35199d5d58cd63
dmunozmauro <40881574+dmunozmauro@users.noreply.github.com>
2019-09-04 00:19:18 -0300

    subir

--- src/app/zapatillas/zapatillas.component.ts
+++ src/app/zapatillas/zapatillas.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Zapatilla } from '../models/zapatilla';
-import { configuracion } from '../models/configuracion'; 
+import { configuracion } from '../models/configuracion';
 
 @Component({
   selector: 'app-zapatillas',
@@ -8,24 +8,39 @@ import { configuracion } from '../models/configuracion';
   styleUrls: ['./zapatillas.component.css']
 })
 export class ZapatillasComponent implements OnInit {
-
+  public title: string;
   public zapatillas: Array<Zapatilla>;
   public color: string;
   public fondo: string;
+  public marcas: String[];
 
   constructor() {
+    this.title = 'Componente de zapatillas';
     this.color = configuracion.color;
     this.fondo = configuracion.fondo;
-    
+
     this.zapatillas = [
       new Zapatilla('Lunar', 1000, 'Nike', 'Negro', true),
       new Zapatilla('Boost', 500, 'Addidas', 'Negro', false),
-      new Zapatilla('Classic', 100, 'Reebok', 'Negro', true)
+      new Zapatilla('Classic', 100, 'Reebok', 'Negro', true),
+      new Zapatilla('Airforce', 1500, 'Nike', 'Roja', true),
     ]
+
+    this.marcas = new Array();
   }
 
   ngOnInit() {
-    console.log(this.zapatillas);
+    this.getMarcas();
   }
+
+  getMarcas() {
+    this.zapatillas.forEach((zapatilla, index) => {
+      if (this.marcas.indexOf(zapatilla.marca) < 0) {
+        this.marcas.push(zapatilla.marca);
+      }
+    });
+    console.log('Las marcas son: ', this.marcas)
+  }
+
 
 }
